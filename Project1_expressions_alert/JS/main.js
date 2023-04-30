20b2b148e7a5a9a8877b653c48476edf27883b63
Yen Culley <yen.nguyen.asia@gmail.com>
2023-04-30 19:26:19 +0100

    Uploaded Basic JavaScript assignment

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+window.alert("Hello, World!")
+var A = "This is a string.";
+document.write(A)
+document.write("Lisa told Bart, \"Knock it off! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br>");
+document.write("I do not like green eggs and ham"+"Sam I am!"+"No I do not!");
